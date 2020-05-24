c2f74a49cefa2f942a1d72ef07f543eaa67b956f
Aex <spryti@qq.com>
2020-05-24 16:06:24 +0800

    废弃对coa-echo的依赖

--- src/die.ts
+++ src/die.ts
@@ -1,11 +1,6 @@
-import { echo } from 'coa-echo'
 import { CoaContextError } from './CoaContextError'
 
 export default new class {
-
-  custom (custom: string, message: string, code: number = 400, mark: number | string = 0): never {
-    throw new CoaContextError({ code, mark, message, custom })
-  }
 
   hint (message: string, code: number = 400, mark: number | string = 0): never {
     throw new CoaContextError({ code, mark, message })
@@ -15,12 +10,11 @@ export default new class {
     throw new CoaContextError({ code, mark, message, stdout: true })
   }
 
-  missing (name: string) {
-    return this.error('缺少' + name + '参数')
+  custom (custom: string, message: string, code: number = 400, mark: number | string = 0): never {
+    throw new CoaContextError({ code, mark, message, custom })
   }
 
-  echo (message: string, code: number = 400, mark: number = 0) {
-    echo.error(message)
-    return this.error(message, code, mark)
+  missing (name: string) {
+    return this.error('缺少' + name + '参数')
   }
 }
